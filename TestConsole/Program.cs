5297585cec57a0600fef443670fc79bb2c02e6a1
OMGeeky <aalaalgmx@gmail.com>
2024-03-31 00:15:44 +0100

    update sample project to compile

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -17,10 +17,10 @@ public partial class Test1 : AtVisualElement
     // [UxmlTrait( "health" , 9 )]
     // public int MyProperty { get; set; }
 
-    [UxmlTrait( "health2" , 8)] public int MyField;
-    [UxmlTrait( "health1" , 8)] public int MyField2{get; set; }
-    [UxmlTrait( "health1" , false)] public bool MyBoolField2;
-    [UxmlTrait( "health1" , "hi")] public string MyStringField2;
+    // [UxmlTrait( "health2" , 8)] public int MyField;
+    // [UxmlTrait( "health1" , 8)] public int MyField2{get; set; }
+    // [UxmlTrait( "health1" , false)] public bool MyBoolField2;
+    // [UxmlTrait( "health1" , "hi")] public string MyStringField2;
     public void Test123()
     {
         Debug.Write( "test" );
